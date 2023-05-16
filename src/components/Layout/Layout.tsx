e77bc49d017abd0ddf999067b64a0c595328ad11
Carlos ALvarez <carlosmail1978@gmail.com>
2023-05-16 23:29:30 +0200

    Fix outlet import

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import Outlet from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import ContainerStyled from "../shared/ContainerStyled";
 import HomePage from "../pages/HomePage/HomePage";
 
